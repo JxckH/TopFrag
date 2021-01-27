5f80d2f5602a6fcce4e6ea1033e64c65b26f82fc
JxckH <47491563+JxckH@users.noreply.github.com>
2021-01-27 17:46:30 -0600

    Add files via upload

---
+++ Site/Bans/layout/footer.php
@@ -0,0 +1,22 @@
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+<script type="text/javascript">
+	$(function () {
+		$('[data-toggle="tooltip"]').tooltip()
+	})
+</script>
+<script type="text/javascript">
+	$(function () {
+	  $('[data-toggle="popover"]').popover()
+	})
+</script>
+<footer class="footer">
+	<div class="container">
+    	<div class="col-md-6 offset-md-3 mt-5">
+    		<!-- <hr> -->
+    		<small class="d-block text-center">GNU General Public License v3.0<br><a href="https://github.com/luiseduardosilva/webAdvancedBan">TopFrag Network | Bans</a> Version 1.0</small>
+    		<small class="d-block text-center">JxckH<br><a href="https://TopFrag.gg">@JxckH</a></small>
+			</div>
+	</div>
+</footer>
---
+++ Site/Bans/layout/page.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once("config.php");
+$page = new Pages();
+if(strpos($_SERVER['REQUEST_URI'], 'history')){
+	$bans = new Historic();
+	$nPage = "history.php?page=";
+}else{
+	$bans = new Ban();
+	$nPage = "index.php?page=";
+}
+?>
+<div class="btn-block">
+	<nav>
+		<ul class="pagination pagination-sm  justify-content-center">
+			<?php $page->listPages();?>
+		</ul>
+	</nav>
+</div>
